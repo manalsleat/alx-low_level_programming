51231bc5c22fcbed19071349d2cb93f13aa280e8
manalsleat <m_sleat@hotmail.com>
2023-04-22 18:35:42 -0700

    Variables

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,10 +2,9 @@
 
 /**
  * main - entry point
- * Description: prints all single digit numbers of base 10 starting from 0, followed by a new line
+ * Description: prints all single digit numbers 10 starting from 0
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
