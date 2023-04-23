79204555f7b2f8b527e6dc57f60419b98b88938d
manalsleat <m_sleat@hotmail.com>
2023-04-22 18:07:13 -0700

    Variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase,
- * Description: followed by a new line
+ * main - entry point
+ *
+ * Description: fprints the alphabet in lowercase, followed by a new line
+ *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char ch;
