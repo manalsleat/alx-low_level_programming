7fd79016aa0e1dd9ad665132b3f1cd42e289fa34
manalsleat <m_sleat@hotmail.com>
2023-05-03 16:02:11 -0700

    Pointers

--- 0x05-pointers_arrays_strings/6-main.c
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -4,8 +4,7 @@
  * main - check the code
  *
  * Return: Always 0.
-*/
-
+ */
 int main(void)
 {
 	char *str;
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,33 +1,28 @@
 #include "main.h"
-
 /**
- * puts2 -  prints every other character of a string
- *	starting with the first character
- *	followed by a new line
- *
- * @str: string input
- *
- * Return: no retun
-*/
-
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
+ */
 void puts2(char *str)
 {
-	int i = 0;
+	int longi = 0;
 	int t = 0;
-	int o;
 	char *y = str;
+	int o;
 
 	while (*y != '\0')
 	{
 		y++;
-		i++;
+		longi++;
 	}
-	t = i - 1;
+	t = longi - 1;
 	for (o = 0 ; o <= t ; o++)
 	{
-		if (i % 2 == 0)
+		if (o % 2 == 0)
 	{
-			_putchar(str[o]);
+		_putchar(str[o]);
 	}
 	}
 	_putchar('\n');
