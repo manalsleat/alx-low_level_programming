ca876da4f9cd161b01df5c687cc16145bd882c8e
manalsleat <m_sleat@hotmail.com>
2023-05-05 05:09:41 -0700

    Variables

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * strcat - concatenates two strings
+ *
+ * @dest: input value
+ * @src: string
+ *
+ * Return: dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
